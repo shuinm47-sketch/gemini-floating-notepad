c5257df64ebbffaa2645d679ce3f34bda6693064
shuinm47-sketch <shuinm47@gmail.com>
2026-02-07 15:51:25 -0500

    Add MainActivity.kt correctly

---
+++ app/src/main/java/com/gemini/floatingnotepad/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.gemini.floatingnotepad
+    
+    import android.content.Intent
+    import android.net.Uri
+    import android.os.Build
+    import android.os.Bundle
+    import android.provider.Settings
+    import android.widget.Toast
+    import androidx.appcompat.app.AppCompatActivity
+    
+    class MainActivity : AppCompatActivity() {
+        private val DRAW_OVER_OTHER_APPS_PERMISSION_REQUEST_CODE = 1234
+    
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            if (checkPermission()) {
+                startFloatingService()
+            } else {
+                requestPermission()
+            }
+        }
+    
+        private fun startFloatingService() {
+            startService(Intent(this, FloatingWidgetService::class.java))
+            finish()
+        }
+    
+        private fun checkPermission(): Boolean {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                Settings.canDrawOverlays(this)
+            } else {
+                true
+            }
+        }
+    
+        private fun requestPermission() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:$packageName")
+                )
+                startActivityForResult(intent, DRAW_OVER_OTHER_APPS_PERMISSION_REQUEST_CODE)
+            }
+        }
+    
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode == DRAW_OVER_OTHER_APPS_PERMISSION_REQUEST_CODE) {
+                if (checkPermission()) {
+                    startFloatingService()
+                } else {
+                    Toast.makeText(this, "Permission required!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    
